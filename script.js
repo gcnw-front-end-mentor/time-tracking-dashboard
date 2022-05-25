f8f96364cfb8eeb3db7a9061132a0515cd342175
gcnw <SkillCatcher@pm.me>
2022-05-25 15:00:46 -0700

    Got JSON stuff working.  Need to do TDD stuff.

---
+++ script.js
@@ -0,0 +1,33 @@
+// RETRIEVE LOCAL JSON DATA
+async function getJSON(){
+    let response = await fetch('./data.json');
+    let data = await response.json()
+    jsonData = data;
+}
+
+// CHANGE HOURS FOR EACH ACTIVITY UPON CLICK
+function updateHours(selection){
+    let timeframe = selection.currentTarget.id;
+    times.forEach(node => node.style.color = '');
+    for(let i=0; i<lastHours.length;i++){
+        lastHours[i].innerHTML = jsonData[i]['timeframes'][timeframe]['previous']+'hrs';
+        currentHours[i].innerHTML = jsonData[i]['timeframes'][timeframe]['current']+'hrs';
+    }
+    selection.currentTarget.style.color = 'white';
+}
+
+const daily = document.getElementById('daily');
+const weekly = document.getElementById('weekly');
+const monthly = document.getElementById('monthly');
+const times = [daily,weekly,monthly];
+const lastHours = document.getElementsByClassName('lastHours');
+const currentHours = document.getElementsByClassName('currentHours');
+
+daily.addEventListener('click', updateHours);
+weekly.addEventListener('click', updateHours);
+monthly.addEventListener('click', updateHours);
+
+// EXECUTE
+
+let jsonData = [];
+getJSON();
---
+++ test/test.js
@@ -0,0 +1,20 @@
+const assert = require('chai').assert;
+const script = require('../script');
+
+describe('Updating when a duration is clicked', function(){
+    describe('Updating Screen when Daily is clicked', function(){
+        describe('Updating the WORK box with daily data', function(){
+            it('Updates current hours to 5', function(){
+                
+            }),
+            it('Updates previous hours to 7', function(){})
+        }),
+        describe('Updating the PLAY box with daily data', function(){}),
+        describe('Updating the STUDY box with daily data', function(){}),
+        describe('Updating the EXERCISE box with daily data', function(){}),
+        describe('Updating the SOCIAL box with daily data', function(){}),
+        describe('Updating the SELFCARE box with daily data', function(){})
+    }),
+    describe('Updating Screen when Weekly is clicked', function(){}),
+    describe('Updating Screen when Monthly is clicked', function(){})
+})
